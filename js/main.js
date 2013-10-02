968dbba91c1607eaeb972dc4c56eb2c53ea02e53
Anne Chu <ahnee.chu@gmail.com>
2013-10-02 00:04:43 -0700

    added win conditions to js

--- js/main.js
+++ js/main.js
@@ -1,39 +1,7 @@
-// function navCells(){
-// 	alert( "nav cells got clicked!"
-// 		);
 
 var gameBoard = ['','','','','','','','',''];
+
 var turn = 'X';
-
-
-
-// for(i=1;i<=9;++i)
-// {
-// 		document.getElementById("box"+i).innerHTML
-// 		// onclick=
-// 		// function(){
-// 		// 	alert("you clicked me!");
-// 		// };
-// }
-
-
-var wins=[[0, 1, 2],
-		  [3, 4, 5],
-		  [6, 7, 8],
-		  [0, 3, 6],
-		  [1, 4, 7],
-		  [2, 5, 8],
-		  [0, 4, 8],
-		  [2, 4, 6]
-		  ]
-
-// }
-
-function buttonClick(){
-		alert( "my button was clicked!"
-			);
-
-}
 
 
 function playGame() {
@@ -54,13 +22,72 @@ function playGame() {
 	} else {
 		alert("Pick another box!" + ind);
 	}
-
+	// insert function to check win conditions
+	winCondition();
 }
+
 
 function fillArray(player) {
 	var box = event.target.id;
 	var ind = box.substring(3) - 1;
 	gameBoard[ind] = player;
+	gameBoard[ind] = fillArray.length;
+	// this is not populating the arrays with "X" or "O" to tell who is winning
+	console.log(gameBoard[ind] = player)
 }
 
+// gameBoard = [0,1,2,3,4,5,6,7,8]
+
+var wins=[[0, 1, 2, "row 1"],
+		  [3, 4, 5, "row 2"],
+		  [6, 7, 8, "row 3"],
+		  [0, 3, 6, "column 1"],
+		  [1, 4, 7, "column 2"],
+		  [2, 5, 8, "column 3"],
+		  [0, 4, 8, "diagonal 1"],
+		  [2, 4, 6, "diagonal 2"]
+		  ]
+
+function winCondition(){
+	for (var i = 0; i < wins.length; i++){
+		if ((gameBoard[wins[i][0]] != "") && (gameBoard[wins[i][0]] == gameBoard[wins[i][1]]) && (gameBoard[wins[i][1]] == gameBoard[wins[i][2]]))
+		{
+			alert(gameBoard[wins[i][0]] + " win! by " + wins[i][3]);
+
+		} 
+
+		// } else {
+		// 	alert("its a tie!");
+		// }
+
+	}
+}
+
+
+
+
+
+
+
+function buttonClick(){
+	alert( "my button was clicked!");
+	
+	// this is what i want the button to do
+	// 	if there is nothing in the game board 
+	// 		btn innerHTML should be ("play game!");
+	// 	else
+	// 		gameBoard array should be cleared 
+	// 		gameBoard[ind] = (array.length == "")
+	// 		btn innerHTML should be ("restart game!")
+	
+}
+
+function pageLoaded(){
+	var box = document.getElementsByClassName("box");
+	// variable box is now identified as the classes named box
+	for(var i = 0; i < box.length; ++i)
+	// for (i=number) that is equal to 0, and number is less than the total numbers of box, add box by 1
+		box[i].onclick = clickDiv;
+
+}
 
